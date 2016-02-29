b73fb7fad1e65f9ac066cd06af57dfcd02b1ed91
suxiongye <suxiongye@21cn.com>
2016-02-29 09:40:05 +0800

    add Dynamic Connectivity

---
+++ src/week1/DynamicConnectivityClient.java
@@ -0,0 +1,27 @@
+package week1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class DynamicConnectivityClient {
+	private static Scanner sc;
+
+	public static void main(String[] args) throws Exception{
+		FileInputStream in = new FileInputStream(new File("tinyUF.txt"));
+		sc = new Scanner(in);
+		int n = sc.nextInt();
+		//初始化动态链接
+		UF uf = new UF(n);
+		while(sc.hasNextLine()){
+			//读取动态链接对
+			int p = sc.nextInt();
+			int q = sc.nextInt();
+			//若未链接则进行链接并输出
+			if(!uf.connected(p, q)){
+				uf.union(p, q);
+				System.out.println(p+" "+q);
+			}
+		}
+	}
+}
---
+++ src/week1/UF.java
@@ -0,0 +1,32 @@
+package week1;
+/**
+ * 该类用于进行动态链接
+ * @author Administrator
+ *
+ */
+public class UF {
+	//构造函数
+	public UF(int n){
+		
+	}
+	
+	//链接p,q
+	public void union(int p, int q){
+		
+	}
+	
+	//查看p,q是否已链接
+	public boolean connected(int p, int q){
+		return false;
+	}
+	
+	//查看p属于哪个集合
+	public int find(int p){
+		return 0;
+	}
+	
+	//查看集合数目
+	public int count(){
+		return 0;
+	}
+}
