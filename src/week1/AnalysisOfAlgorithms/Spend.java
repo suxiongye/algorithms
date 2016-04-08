ef66dced5d744d210d31c9e600ac9dc0d905da91
suxiongye <suxiongye@21cn.com>
2016-04-08 09:19:32 +0800

    add the function of cal running time

---
+++ src/week1/AnalysisOfAlgorithms/Spend.java
@@ -0,0 +1,18 @@
+package week1.AnalysisOfAlgorithms;
+
+public class Spend {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// watch the running time
+		long start = System.currentTimeMillis();
+
+		for(int i = 0; i < 100000; i++){
+			
+		}
+		long now = System.currentTimeMillis();
+		System.out.println((now - start) / 1000.0);
+	}
+
+}
